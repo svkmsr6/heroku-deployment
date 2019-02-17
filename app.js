b5f9e34e67ea8f12effbf262f500c42a59f987fb
Souvik Misra <svkmsr6@gmail.com>
2019-02-17 10:27:12 +0530

    error handling changes

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status:err.status || 500,
+    msg:'An internal server error has occurred'
+  });
 });
 
 module.exports = app;
